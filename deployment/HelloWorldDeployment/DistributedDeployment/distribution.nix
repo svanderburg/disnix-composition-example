1da495991e3461933995e920fa57284171e04c8c
Sander van der Burg <svanderburg@gmail.com>
2009-06-17 11:55:50 +0000

    Removed bad examples

--- deployment/HelloWorldDeployment/DistributedDeployment/distribution.nix
+++
@@ -1,6 +0,0 @@
-{services, infrastructure}:
-
-[
-    { service = services.HelloService; target = infrastructure.itchy; }
-    { service = services.HelloWorldService; target = infrastructure.scratchy; }
-]
