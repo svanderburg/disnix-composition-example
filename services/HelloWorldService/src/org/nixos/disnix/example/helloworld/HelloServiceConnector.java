7b341cf2e4617934efeb50321668306fd9fdfa30
Sander van der Burg <svanderburg@gmail.com>
2009-06-30 18:26:39 +0000

    Added some documentation

--- services/HelloWorldService/src/org/nixos/disnix/example/helloworld/HelloServiceConnector.java
+++ services/HelloWorldService/src/org/nixos/disnix/example/helloworld/HelloServiceConnector.java
@@ -6,12 +6,23 @@ import org.apache.axis2.addressing.*;
 import org.apache.axis2.client.*;
 import org.apache.axis2.rpc.client.*;
 
+/**
+ * Provides an one-on-one interface to the Hello WebService
+ */
 public class HelloServiceConnector
 {
+	/** Service client that sends all requests to the Hello WebService */
 	private RPCServiceClient serviceClient;
 	
+	/** Namespace of all operation names */
 	private static final String NAME_SPACE = "http://helloworld.example.disnix.nixos.org";
 	
+	/**
+	 * Creates a new HelloService connector instance.
+	 * 
+	 * @param serviceURL URL of the target end point of the HelloService
+	 * @throws Exception If some error occurs
+	 */
 	public HelloServiceConnector(String serviceURL) throws Exception
 	{
 		serviceClient = new RPCServiceClient();
@@ -20,6 +31,12 @@ public class HelloServiceConnector
 		options.setTo(targetEPR);
 	}
 	
+	/**
+	 * Retrieves the 'hello' string from the Hello WebService
+	 * 
+	 * @return Hello string
+	 * @throws AxisFault If the request fails
+	 */
 	public String getHello() throws AxisFault
 	{
 		QName operation = new QName(NAME_SPACE, "getHello");
--- services/HelloWorldService/src/org/nixos/disnix/example/helloworld/HelloWorldService.java
+++ services/HelloWorldService/src/org/nixos/disnix/example/helloworld/HelloWorldService.java
@@ -1,10 +1,21 @@
 package org.nixos.disnix.example.helloworld;
 import java.util.*;
 
+/**
+ * A trivial web service which only purpose is to say 'Hello world!'.
+ * This web service invokes a HelloService instance to retrieve the 'Hello' string,
+ * then it uses the retrieved string to compose the phrase 'Hello world!'.
+ */
 public class HelloWorldService
 {
+	/** Interface that sends requests to the HelloService */
 	private HelloServiceConnector connector;
 	
+	/**
+	 * Creates a new HelloWorldService instance.
+	 * 
+	 * @throws Exception If some error occurs
+	 */
 	public HelloWorldService() throws Exception
 	{
 		/* Read the target end point reference of the hello service from the properties file */
@@ -16,6 +27,12 @@ public class HelloWorldService
 		connector = new HelloServiceConnector(targetEPR);
 	}
 	
+	/**
+	 * Returns the string 'Hello world!'
+	 * 
+	 * @return Hello world string
+	 * @throws Exception If some error occurs
+	 */
 	public String getHelloWorld() throws Exception
 	{
 		return connector.getHello() + " world!";
