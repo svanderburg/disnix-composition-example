5cc34982553bdc02c4ce7db67526c9cf03bb46cf
Sander van der Burg <svanderburg@gmail.com>
2009-06-30 15:18:53 +0000

    Added some documentation

--- services/HelloService/src/org/nixos/disnix/example/helloworld/HelloService.java
+++ services/HelloService/src/org/nixos/disnix/example/helloworld/HelloService.java
@@ -1,7 +1,15 @@
 package org.nixos.disnix.example.helloworld;
 
+/**
+ * A trivial example web service which only purpose is to say 'Hello'.
+ */
 public class HelloService
 {
+	/**
+	 * Returns the string 'Hello'
+	 * 
+	 * @return Hello string
+	 */
 	public String getHello()
 	{
 		return "Hello";
