8d68cced0ef7ff58ecdc648c628de5802b0c0fce
Sander van der Burg <svanderburg@gmail.com>
2010-04-28 13:07:35 +0000

    Adapted the HelloDBService to use the JDBC connection pool.
    The advantage of using this method is that connections are only used if necessary and it offers some robustness in restoring connections in case of a failure.

--- services/HelloDBService/src/org/nixos/disnix/example/helloworld/HelloDBService.java
+++ services/HelloDBService/src/org/nixos/disnix/example/helloworld/HelloDBService.java
@@ -4,78 +4,69 @@ import java.sql.*;
 import java.util.*;
 import javax.naming.*;
 import javax.sql.*;
-import org.apache.axis2.*;
-import org.apache.axis2.context.*;
-import org.apache.axis2.service.*;
 
 /**
  * A trivial example web service which only purpose is to say 'Hello'.
  * This HelloService variant uses a database in which the 'Hello' string is stored.
  */
-public class HelloDBService implements Lifecycle
+public class HelloDBService
 {
-	/** Connection to the database */
-	private Connection connection;
+	private String jndiName;
 	
-	/**
-	 * @see Lifecycle#init(ServiceContext)
-	 */
-	@Override
-	public void init(ServiceContext context) throws AxisFault
+	public HelloDBService() throws Exception
 	{
-		try
-		{
-			/* Read the JNDI name of the database from the properties file */
-			Properties props = new Properties();
-			props.load(getClass().getResourceAsStream("hellodb.properties"));
-			String jndiName = props.getProperty("JNDI");
-			
-			/* 
-			 * Fetch the database connection from the application server
-			 * by using the JNDI interface
-			 */
-			InitialContext ctx = new InitialContext();
-			DataSource ds = (DataSource)ctx.lookup(jndiName);
-			connection = ds.getConnection();
-		}
-		catch(Exception ex)
-		{
-			/* Throw an error to the user if something goes wrong with the initialization */
-			throw new AxisFault(ex.toString());
-		}
+		/* Read the JNDI name of the database from the properties file */
+		Properties props = new Properties();
+		props.load(getClass().getResourceAsStream("hellodb.properties"));
+		jndiName = props.getProperty("JNDI");
 	}
 	
-	/**
-	 * @see Lifecycle#destroy(ServiceContext)
-	 */
-	@Override
-	public void destroy(ServiceContext context)
-	{
-		try
-		{
-			/* Cleanup the database connection and return it to the connection pool */			
-			connection.close();
-		}
-		catch(Exception ex)
-		{
-			ex.printStackTrace();
-		}
+	/** 
+	 * Fetch the database connection from the application server
+	 * by using the JNDI interface
+	 * 
+	 * @return Connection Database connection from the connection pool
+	 */	
+	private Connection retrieveConnection() throws Exception
+	{					
+		InitialContext ctx = new InitialContext();
+		DataSource ds = (DataSource)ctx.lookup(jndiName);
+		return ds.getConnection();
 	}
-
+		
 	/**
 	 * Returns the string 'Hello', which is queried from the database
 	 * 
 	 * @return Hello string
 	 */
 	public String getHello() throws Exception
-	{
-		/* Query the hello string from the database */
-		Statement stmt = connection.createStatement();
-		ResultSet result = stmt.executeQuery("select hello from hello");
+	{		
+		/* Fetch a connection from the connection pool */
+		Connection connection = retrieveConnection();
 		
-		if(result.next())
-			return result.getString(1); // Return the queried hello string
-		else
-			throw new Exception("No records found"); // Return an exception if there are no records in the database
+		try
+		{
+			/* Query the hello string from the database */
+			Statement stmt = connection.createStatement();
+			ResultSet result = stmt.executeQuery("select hello from hello");
+			String hello = null;
+			
+			if(result.next())
+				hello = result.getString(1); // Retrieve the queried hello string		
+			
+			if(hello == null)
+				throw new Exception("No records found"); // Return an exception if there are no records in the database
+			else
+				return hello; // Return the hello string
+		}
+		catch(Exception ex)
+		{
+			throw ex;
+		}
+		finally
+		{
+			/* Release the connection to the connection pool */
+			connection.close();
+		}
 	}
 }
