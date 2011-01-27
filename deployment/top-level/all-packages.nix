44b9de90142670ffde84a82290be20edd1d568ac
Sander van der Burg <svanderburg@gmail.com>
2011-01-27 13:17:20 +0000

    Remove ugly with pkgs;

--- deployment/top-level/all-packages.nix
+++ deployment/top-level/all-packages.nix
@@ -9,50 +9,52 @@
 , pkgs
 }:
 
-with pkgs;
-
 rec {
   HelloService = import ../pkgs/HelloService {
-    inherit stdenv apacheAnt axis2;
+    inherit (pkgs) stdenv apacheAnt axis2;
   };
   
   HelloWorldService = import ../pkgs/HelloWorldService {
-    inherit stdenv apacheAnt axis2;
+    inherit (pkgs) stdenv apacheAnt axis2;
   };
 
   HelloWorld = import ../pkgs/HelloWorld {
-    inherit stdenv apacheAnt axis2;
+    inherit (pkgs) stdenv apacheAnt axis2;
   };
   
   LookupConfig = if distribution == null || services == null then null else import ../pkgs/LookupConfig {
-    inherit stdenv libxslt distribution services;
+    inherit (pkgs) stdenv libxslt;
+    inherit distribution services;
   };
   
   LookupService = import ../pkgs/LookupService {
-    inherit stdenv apacheAnt axis2 LookupConfig;
+    inherit (pkgs) stdenv apacheAnt axis2;
+    inherit LookupConfig;
   };
 
   HelloWorldService2 = import ../pkgs/HelloWorldService2 {
-    inherit stdenv apacheAnt axis2;
+    inherit (pkgs) stdenv apacheAnt axis2;
   };
 
   HelloWorld2 = import ../pkgs/HelloWorld2 {
-    inherit stdenv apacheAnt axis2;
+    inherit (pkgs) stdenv apacheAnt axis2;
   };
   
   HelloMySQLDB = import ../pkgs/HelloMySQLDB {
-    inherit stdenv;
+    inherit (pkgs) stdenv;
   };    
   
   HelloDBService = import ../pkgs/HelloDBService {
-    inherit stdenv apacheAnt axis2;
+    inherit (pkgs) stdenv apacheAnt axis2;
   };
   
   HelloDBServiceWrapper = import ../pkgs/HelloDBService/wrapper.nix {
-    inherit stdenv HelloDBService;
+    inherit (pkgs) stdenv;
+    inherit HelloDBService;
   };
   
   LookupService2 = import ../pkgs/LookupService2 {
-    inherit stdenv apacheAnt axis2 LookupConfig;
+    inherit (pkgs) stdenv apacheAnt axis2;
+    inherit LookupConfig;
   };
 }
