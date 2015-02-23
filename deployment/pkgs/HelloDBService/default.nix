9c641c469bf2e585ba8fc35d22eadc9a2381d135
Sander van der Burg <svanderburg@gmail.com>
2015-02-23 14:29:46 +0000

    Fix compilation of HelloDBService

--- deployment/pkgs/HelloDBService/default.nix
+++ deployment/pkgs/HelloDBService/default.nix
@@ -1,9 +1,9 @@
-{stdenv, apacheAnt, axis2}:
+{stdenv, apacheAnt, jdk, axis2}:
 
 stdenv.mkDerivation {
   name = "HelloDBService";
   src = ../../../services/HelloDBService;
-  buildInputs = [ apacheAnt ];
+  buildInputs = [ jdk apacheAnt ];
   AXIS2_LIB = "${axis2}/lib";
   AXIS2_WEBAPP = "${axis2}/webapps/axis2";
   buildPhase = "ant generate.war";
