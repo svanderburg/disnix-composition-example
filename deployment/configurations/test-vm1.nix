f01b8b2ee0465f40a4dc85ac96c527c89d94fef2
Sander van der Burg <svanderburg@gmail.com>
2022-03-13 23:34:08 +0100

    Remove obsolete Tomcat configuration properties

--- deployment/configurations/test-vm1.nix
+++ deployment/configurations/test-vm1.nix
@@ -15,11 +15,8 @@
       enable = true;
       commonLibs = [ "${pkgs.mysql_jdbc}/share/java/mysql-connector-java.jar" ];
       catalinaOpts = "-Xms64m -Xmx256m";
-      package = pkgs.tomcat9;
     };
   };
-
-  users.users.tomcat.group = "tomcat";
 
   networking.firewall.allowedTCPPorts = [ 8080 ];
 
--- deployment/configurations/test-vm2-mysql.nix
+++ deployment/configurations/test-vm2-mysql.nix
@@ -13,18 +13,15 @@
 
     mysql = {
       enable = true;
-      package = pkgs.mysql;
+      package = pkgs.mariadb;
     };
 
     tomcat = {
       enable = true;
       commonLibs = [ "${pkgs.mysql_jdbc}/share/java/mysql-connector-java.jar" ];
       catalinaOpts = "-Xms64m -Xmx256m";
-      package = pkgs.tomcat9;
     };
   };
-
-  users.users.tomcat.group = "tomcat";
 
   networking.firewall.allowedTCPPorts = [ 3306 8080 ];
 
